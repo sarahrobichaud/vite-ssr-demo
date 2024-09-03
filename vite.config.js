b37bfd6a961b950e6f404318476aab64e4967a55
Sarah Robichaud <robichaudsarah@outlook.com>
2024-09-03 14:12:36 -0300

    Updated vite config for assets

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,21 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()]
-})
+  base: "./",
+  plugins: [
+    {
+      name: "adjust-asset-paths",
+      enforce: "post",
+      transform(code, id) {
+        // Example: Replace '/assets/' with './assets/' in all JS files
+        if (id.endsWith(".js")) {
+          return code.replace(/\/assets\//g, "./assets/");
+        }
+        return code;
+      },
+    },
+    react(),
+  ],
+});
